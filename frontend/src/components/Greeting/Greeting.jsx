bfc682b39adf2be121ececaf6ae7044b4342890d
Jefersson Aldemar Velasquez Mendoza <jeferssonveme.97@gmail.com>
2025-07-09 22:52:41 -0500

    mejorar estilos y estructura de componentes: agregar clases y ajustar estilos en Cards, Contact, Projects y Skills; implementar manejo de visibilidad en el componente Header

--- frontend/src/components/Greeting/Greeting.jsx
+++ frontend/src/components/Greeting/Greeting.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import '../../styles/reset.css'
 import './Greeting.css'
-
+import '../../styles/buttons.css'
 export default function Greeting() {
   return (
-    <section className='Greeting'>
-        <div className='Greeting__container'>
-            <div className='Greeting__text'>
-              <h1>Welcome to my portfolio!</h1>
+    <section className='greeting'>
+            <div className='greeting__text'>
+              <h2>Welcome to my portfolio!</h2>
               <p>Explore my projects and skills.</p>
               <p>Feel free to contact me for any inquiries.</p>
             </div>
-            <div className='Greeting__links'>
-              <a href=""><span>Curricul pdf</span></a>
-            </div>
-        </div>
-        <picture Greeting__img>
-            <img src="https://img-database.jeferssonvm97.com/img_projects/1jeferssonSn.png" alt="Foto Personal" />
+            <section class="button-container greeting__links">
+              <button>
+                <span class="text"><a href="https://jeferssonvm97.com/Curriculum/2024-08-22_Curriculum.pdf">Curriculum</a></span>
+                <span class="line -right"></span>
+                <span class="line -top"></span>
+                <span class="line -left"></span>
+                <span class="line -bottom"></span>
+              </button>
+            </section>
+
+        <picture className='greeting__img'>
+            <img src="https://img-database.jeferssonvm97.com/img_projects/1jeferssonSn.png" alt="Foto Personal" loading="lazy"/>
         </picture>
     </section>
   )
 }
+
